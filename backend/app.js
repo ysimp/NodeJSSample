761a9a25de763579d768df9285473bd72b389aff
ysimp <yaya.simpara.5@gmail.com>
2024-05-17 15:20:04 +0200

    add router

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Thing = require('./models/thing');
+const stuffRoutes = require('./routes/stuff');
 
-mongoose.connect('mongodb+srv://ysi:<PassWord>@ysinodejscluster.axeiczw.mongodb.net/?retryWrites=true&w=majority&appName=ysiNodeJsCLuster',
+  mongoose.connect('mongodb+srv://ysi:<PassWord>@ysinodejscluster.axeiczw.mongodb.net/?retryWrites=true&w=majority&appName=ysiNodeJsCLuster',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -20,43 +20,6 @@ app.use((req, res, next) => {
     next();
   });
 
-
-app.post('/api/stuff', (req, res, next) =>{
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-    .then(()=> res.status(201).json({message: 'Objet enregistré'}))
-    .catch(()=> res.status(400).json({error}));
-});
-
-app.put('/api/stuff/:id',(req, res, next)=>{
-    Thing.updateOne(
-        {_id: req.params.id}, {...req.body, _id: req.params.id}
-    ).then(()=> res.status(200).json({message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({error}));
-});
-
-app.delete('/api/stuff/:id',(req, res, next)=>{
-    Thing.deleteOne(
-        {_id: req.params.id}
-    ).then(()=> res.status(200).json({message: 'Objet supprimé!'}))
-    .catch(error => res.status(400).json({error}));
-});
-
-app.get('/api/stuff/:id',(req, res, next)=>{
-    Thing.findOne({_id: req.params.id})
-    .then(thing => res.status(200).json(thing))
-    .catch(()=> res.status(400).json({error}));
-    
-});
-
-  //app.use intercept all request
-app.get('/api/stuff',(req, res, next)=>{
-    Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(()=> res.status(400).json({error}));
-});
+  app.use('/api/stuff',stuffRoutes);
 
 module.exports = app;
